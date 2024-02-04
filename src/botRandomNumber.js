d7b0f4a57047476a33504e1e162d9cf5d181e6d3
Maxi-Nebula <madhuravichandranofficial@gmail.com>
2024-02-03 22:48:38 -0700

    added the already attacked position to avoid bot choosing the same position again from the player board

--- src/botRandomNumber.js
+++ src/botRandomNumber.js
@@ -1,9 +1,9 @@
 /** @format */
 
-import { indexOf } from "lodash";
-
 function chooseRandomNumber(availablePositions) {
   //const allBotGrids = document.querySelectorAll(".grid-item1");
+  console.log("choose random number");
+  console.log(availablePositions);
 
   const random = Math.floor(Math.random() * availablePositions.length);
   let gridValue = availablePositions[random];
--- src/playerAttack.js
+++ src/playerAttack.js
@@ -3,6 +3,7 @@ import messageBoard from "./messageBoard";
 import enemyAttack from "./enemyAttack";
 import chooseRandomNumber from "./botRandomNumber";
 import logAttackedPositions from "./logAttackedPositions";
+import { indexOf } from "lodash";
 
 function playerAttack(grid, logBotChosenPostions, allPlayerpositions) {
   let isEnemyAttacked = false;
@@ -20,19 +21,31 @@ function playerAttack(grid, logBotChosenPostions, allPlayerpositions) {
   } else if (isEnemyAttacked == false) {
     messageBoard("It's a miss..");
   }
-  function removeTargetedPlayerPosition(allPlayerpositions, targetedPosition) {
+  /* function removeTargetedPlayerPosition(allPlayerpositions, targetedPosition) {
     return allPlayerpositions.filter(function (ele) {
       return ele != targetedPosition;
     });
+  }*/
+
+  function removeTargetedPlayerPosition(
+    allPlayerpositions,
+    targetedPlayerPosition
+  ) {
+    //  console.log(allPlayerpositions.indexOf(targetedPlayerPosition));
+    allPlayerpositions.splice(
+      allPlayerpositions.indexOf(targetedPlayerPosition),
+      1
+    );
+    return allPlayerpositions;
   }
   const targetedPlayerPosition = chooseRandomNumber(allPlayerpositions);
+  const alreadyAttackedPositions = logAttackedPositions(targetedPlayerPosition);
   console.log(targetedPlayerPosition);
+
   allPlayerpositions = removeTargetedPlayerPosition(
     allPlayerpositions,
     targetedPlayerPosition
   );
-  logAttackedPositions(targetedPlayerPosition);
-  console.log(allPlayerpositions);
 
   return isEnemyAttacked;
 }
